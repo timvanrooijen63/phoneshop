fc3427a9bf9628c507bf3bdf16fe7c972f47dfdf
Tim <Tim@PC13>
2016-05-15 13:56:53 +0200

    Added jenkins file

---
+++ PhoneShopTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PhoneShopTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
